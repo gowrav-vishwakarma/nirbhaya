44e0f01df10968ca36e9a86f67033fb260cb7f7d
bhashkar <bhashkar2600@gmail.com>
2024-12-16 15:24:27 +0530

    wip

--- src/pages/Account/ProfilePage.vue
+++ src/pages/Account/ProfilePage.vue
@@ -3,25 +3,53 @@
     <div class="profile-content">
       <q-card flat class="profile-card">
         <q-card-section>
-          <!-- <div class="text-h6 text-weight-bold q-mb-md">
-            {{ $t('common.profileSettings') }}
-          </div> -->
           <LanguageSelector class="q-mb-lg" />
 
           <q-form @submit.prevent="handleSubmit">
-            <!-- Basic profile information -->
             <div class="row q-col-gutter-sm">
-              <div class="col-12 col-sm-6 q-py-none">
-                <q-input class="" v-model="values.name" :label="$t('common.name')" outlined dense :error="!!errors.name"
-                  :error-message="errors.name?.join('; ')" :rules="[(val) => !!val || $t('common.nameRequired')]" />
+              <!-- Name Input -->
+              <div class="col-12 col-sm-6 q-py-none custom-input">
+                <label>{{ $t('common.name') }}</label>
+                <q-input 
+                  v-model="values.name" 
+                  :error="!!errors.name"
+                  :error-message="errors.name?.join('; ')" 
+                  :rules="[(val) => !!val || $t('common.nameRequired')]"
+                  filled
+                  class="custom-radius"
+                  bg-color="pink-1"
+                  dense
+                  hide-bottom-space
+                />
               </div>
-              <div class="col-12 col-sm-6 q-py-none">
-                <q-input class="q-pb-md" v-model="values.phoneNumber" :label="$t('common.mobileNumber')" outlined dense
-                  readonly disable />
+
+              <!-- Phone Number Input -->
+              <div class="col-12 col-sm-6 q-py-none custom-input">
+                <label>{{ $t('common.mobileNumber') }}</label>
+                <q-input 
+                  v-model="values.phoneNumber"
+                  filled
+                  class="custom-radius"
+                  bg-color="pink-1"
+                  dense
+                  disable
+                  readonly
+                  hide-bottom-space
+                />
               </div>
-              <div class="col-12 col-sm-6 q-py-none">
-                <q-input @keydown.prevent class="q-pb-md" outlined dense v-model="values.dob" :label="$t('common.dob')"
-                  mask="date" :rules="['date']">
+
+              <!-- DOB Input -->
+              <div class="col-12 col-sm-6 q-py-none custom-input">
+                <label>{{ $t('common.dob') }}</label>
+                <q-input 
+                  v-model="values.dob"
+                  mask="date"
+                  filled
+                  class="custom-radius"
+                  bg-color="pink-1"
+                  dense
+                  hide-bottom-space
+                >
                   <template v-slot:append>
                     <q-icon name="event" class="cursor-pointer">
                       <q-popup-proxy cover transition-show="scale" transition-hide="scale">
@@ -35,132 +63,106 @@
                   </template>
                 </q-input>
               </div>
-              <div class="col-12 col-sm-6 q-py-none">
-                <q-select class="q-pb-md" v-model="values.state" :options="stateOptions" :label="$t('common.state')"
-                  outlined dense clearable use-input input-debounce="0" @filter="filterStates" :error="!!errors.state"
-                  :error-message="errors.state?.join('; ')" @update:model-value="handleStateChange" />
+
+              <!-- State Input -->
+              <div class="col-12 col-sm-6 q-py-none custom-input">
+                <label>{{ $t('common.state') }}</label>
+                <q-select 
+                  v-model="values.state"
+                  :options="stateOptions"
+                  filled
+                  class="custom-radius"
+                  bg-color="pink-1"
+                  dense
+                  clearable
+                  use-input
+                  input-debounce="0"
+                  @filter="filterStates"
+                  :error="!!errors.state"
+                  :error-message="errors.state?.join('; ')"
+                  @update:model-value="handleStateChange"
+                  hide-bottom-space
+                />
               </div>
-              <div class="col-12 col-sm-6 q-py-none">
-                <SearchCity v-model="values.city" :error="errors.city?.join('; ')" :selected-state="values.state || ''"
-                  @update:modelValue="handleCitySelection" :disabled="!values.state" :key="values.state" />
+
+              <!-- City Input -->
+              <div class="col-12 col-sm-6 q-py-none custom-input">
+                <label>{{ $t('common.city') }}</label>
+                <SearchCity
+                  v-model="values.city"
+                  :error="errors.city?.join('; ')"
+                  :selected-state="values.state || ''"
+                  @update:modelValue="handleCitySelection"
+                  :disabled="!values.state"
+                  :key="values.state"
+                />
               </div>
-              <div class="col-12 col-sm-6 q-py-none">
-                <q-select v-model="values.userType" :options="userTypeOptions" :label="$t('common.userType')" outlined
-                  dense :error="!!errors.userType" :error-message="errors.userType?.join('; ')" />
+
+              <!-- User Type Input -->
+              <div class="col-12 col-sm-6 q-py-none custom-input">
+                <label>{{ $t('common.userType') }}</label>
+                <q-select 
+                  v-model="values.userType"
+                  :options="userTypeOptions"
+                  filled
+                  class="custom-radius"
+                  bg-color="pink-1"
+                  dense
+                  :error="!!errors.userType"
+                  :error-message="errors.userType?.join('; ')"
+                  hide-bottom-space
+                />
               </div>
-              <div class="col-12 q-py-none">
-                <q-select v-model="values.profession" :options="professionOptions" :label="$t('common.profession')"
-                  outlined dense :error="!!errors.profession" :error-message="errors.profession?.join('; ')" map-options
-                  emit-value option-value="value" option-label="label" />
+
+              <!-- Profession Input -->
+              <div class="col-12 q-py-none custom-input">
+                <label>{{ $t('common.profession') }}</label>
+                <q-select 
+                  v-model="values.profession"
+                  :options="professionOptions"
+                  filled
+                  class="custom-radius"
+                  bg-color="pink-1"
+                  dense
+                  :error="!!errors.profession"
+                  :error-message="errors.profession?.join('; ')"
+                  map-options
+                  emit-value
+                  option-value="value"
+                  option-label="label"
+                  hide-bottom-space
+                />
               </div>
-              <div class="col-12 q-py-none">
-                <q-input v-model="values.referredBy" :label="$t('common.referredBy')" outlined dense
-                  :disable="isReferralIdStored" :readonly="isReferralIdStored" :error="!!errors.referredBy"
-                  :error-message="errors.referredBy?.join('; ')" />
+
+              <!-- Referred By Input -->
+              <div class="col-12 q-py-none custom-input">
+                <label>{{ $t('common.referredBy') }}</label>
+                <q-input 
+                  v-model="values.referredBy"
+                  filled
+                  class="custom-radius"
+                  bg-color="pink-1"
+                  dense
+                  :error="!!errors.referredBy"
+                  :error-message="errors.referredBy?.join('; ')"
+                  :disable="isReferralIdStored"
+                  hide-bottom-space
+                />
               </div>
             </div>
 
-            <!-- Emergency contacts -->
-            <!-- <div class="q-mt-lg"> -->
-            <!-- <div class="text-subtitle1 text-weight-bold q-mb-sm">
-                {{ $t('common.emergencyContacts') }}
-                <q-icon :name="$t('common.icons.help')" size="xs" class="q-ml-sm">
-                  <q-tooltip>{{
-                    $t('common.emergencyContactsHelp')
-                    }}</q-tooltip>
-                </q-icon>
-              </div> -->
-
-            <!-- Button group for Add Emergency Contact and Emergency Contact Requests -->
-            <!-- <div class="row q-col-gutter-sm q-mb-md">
-                <div class="col-12">
-                  <q-btn-group class="full-width">
-                    <q-btn v-if="values.emergencyContacts.length < 3" @click="addEmergencyContact" color="primary"
-                      :icon="$t('common.icons.addCircle')" :label="$t('common.addEmergencyContact')" no-caps
-                      class="full-width" />
-                    <q-btn @click="openEmergencyContactRequests" color="secondary" :icon="$t('common.icons.contacts')"
-                      :label="$t('common.emergencyContactRequests')" no-caps class="full-width" />
-                  </q-btn-group>
-                </div>
-              </div> -->
-
-            <!-- <q-list bordered separator> -->
-            <!-- <q-item v-for="(contact, index) in values.emergencyContacts" :key="index">
-                  <q-item-section>
-                    <q-input v-model="contact.contactName" :label="$t('common.name')" dense outlined class="q-mb-sm"
-                      :rules="[
-                        (val) => !!val || $t('common.contactNameRequired'),
-                      ]" />
-                    <q-input v-model="contact.contactPhone" :label="$t('common.mobileNumber')" dense outlined
-                      class="q-mb-sm" :rules="[
-                        (val) => !!val || $t('common.contactNumberRequired'),
-                      ]" @blur="validatePhoneNumber(contact.contactPhone, index)"
-                      :error="!!errors[`emergencyContact${index}`]" :error-message="errors[`emergencyContact${index}`]?.join('; ')
-                        " />
-                    <q-chip :color="contact.consentGiven ? 'positive' : 'warning'" text-color="white"
-                      :icon="contact.consentGiven ? 'check_circle' : 'warning'" size="sm">
-                      {{
-                        contact.consentGiven
-                          ? $t('common.approved')
-                          : $t('common.pendingApproval')
-                      }}
-                    </q-chip>
-                  </q-item-section>
-                  <q-item-section side>
-                    <q-btn flat round color="negative" :icon="$t('common.icons.delete')"
-                      @click="removeEmergencyContact(index)" />
-                  </q-item-section>
-                </q-item> -->
-            <!-- </q-list> -->
-            <!-- <p v-if="!hasEmergencyContacts" class="text-negative q-mt-sm">
-                {{ $t('common.atLeastOneEmergencyContactRequired') }}
-              </p> -->
-            <!-- </div> -->
-
-            <!-- SOS Settings -->
-            <!-- <div v-if="isNavigatorMediaSupported" class="q-mt-lg">
-              <div class="text-subtitle1 text-weight-bold q-mb-sm">
-                {{ $t('common.sosSettings') }}
-              </div>
-              <q-list bordered>
-                <q-item tag="label" v-ripple>
-                  <q-item-section>
-                    <q-item-label>{{
-                      $t('common.startAudioVideoRecordOnSos')
-                    }}</q-item-label>
-                  </q-item-section>
-                  <q-item-section side>
-                    <q-toggle v-model="values.startAudioVideoRecordOnSos" />
-                  </q-item-section>
-                </q-item>
-                <q-item v-if="STREAM_SAVE" tag="label" v-ripple>
-                  <q-item-section>
-                    <q-item-label>{{
-                      $t('common.streamAudioVideoOnSos')
-                    }}</q-item-label>
-                  </q-item-section>
-                  <q-item-section side>
-                    <q-toggle v-model="values.streamAudioVideoOnSos" />
-                  </q-item-section>
-                </q-item>
-                <q-item tag="label" v-ripple>
-                  <q-item-section>
-                    <q-item-label>{{
-                      $t('common.broadcastAudioOnSos')
-                    }}</q-item-label>
-                  </q-item-section>
-                  <q-item-section side>
-                    <q-toggle v-model="values.broadcastAudioOnSos" color="grey" :disable="true" />
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </div> -->
-
-            <!-- Submit button -->
+            <!-- Submit Button -->
             <div class="row q-col-gutter-md q-mt-none" style="margin-top: -15px;">
               <div class="col-12">
-                <q-btn type="submit" :loading="isLoading" color="primary" class="full-width" :disable="!isFormValid"
-                  no-caps>
+                <q-btn 
+                  type="submit" 
+                  :loading="isLoading" 
+                  color="primary" 
+                  class="full-width next-button q-mt-xs" 
+                  :disable="!isFormValid"
+                  no-caps
+                  style="border-radius: 10px !important; height: 40px;"
+                >
                   <b>{{ $t('common.saveChanges') }}</b>
                 </q-btn>
               </div>
@@ -180,7 +182,7 @@ import { useUserStore } from 'src/stores/user-store';
 import { api } from 'src/boot/axios';
 import { useForm } from 'src/qnatk/composibles/use-form';
 import LanguageSelector from 'src/components/LanguageSelector.vue';
-import SearchCity from 'src/components/SearchTwoCity.vue';
+import SearchCity from 'src/components/SearchCity.vue';
 import type { QSelectFilterFn } from 'quasar';
 
 const { t } = useI18n();
@@ -588,5 +590,38 @@ const isReferralIdStored = computed(() => {
 .profile-content {
   max-width: 600px;
   margin: 0 auto;
+}
+
+.custom-input {
+  margin-bottom: 20px;
+}
+
+.custom-input label {
+  display: block;
+  margin-bottom: 4px;
+  font-size: 0.9rem;
+  color: #666;
+}
+
+.next-button {
+  width: 100%;
+  border-radius: 10px;
+}
+
+/* Add this new style for custom border radius */
+:deep(.custom-radius) .q-field__control {
+  border-radius: 10px !important;
+  height: 45px;
+}
+
+:deep(.custom-radius) .q-field__marginal {
+  height: 56px;
+  border-radius: 20px;
+}
+
+/* Optional: If you want to style the inner input area as well */
+:deep(.custom-radius) .q-field__native,
+:deep(.custom-radius) .q-field__input {
+  border-radius: 20px;
 }
 </style>
