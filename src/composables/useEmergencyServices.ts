0d53a8521f28c4f44ab993316edf1743f1e5c2df
saddam chouhan <saddam.c@frendy.in>
2025-01-23 12:40:11 +0530

    working on user registration page

--- src/composables/useEmergencyServices.ts
+++ src/composables/useEmergencyServices.ts
@@ -1,8 +1,8 @@
 import { ref } from 'vue';
-import { useI18n } from 'vue-i18n';
+// import { useI18n } from 'vue-i18n';
 
 export function useEmergencyServices() {
-  const { t } = useI18n();
+  // const { t } = useI18n();
 
   const emergencyServices = ref([
     {
