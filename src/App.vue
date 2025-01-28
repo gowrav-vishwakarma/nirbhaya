ccb92a2f773e5b0eadb98e0f0f33959d3c4dd5df
sinharakesh <rksinha.btech@gmail.com>
2025-01-28 19:00:04 +0530

    smartlook added

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,16 @@
   <router-view />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Smartlook } from '@awesome-cordova-plugins/smartlook';
+import { onMounted } from 'vue';
+
+// Set up Smartlook with your API key
+onMounted(async () => {
+  Smartlook.setProjectKey({ key: '056d4905ebd5ce473f5ad5fa953788424054cd60' });
+  Smartlook.start();
+  console.log('smartlook hello');
+  const isRecording = await Smartlook.isRecording();
+  console.log('isRecording', isRecording);
+});
+</script>
