2dbe9c04974a332969f372f960d4447c9ada80ad
sinharakesh <rksinha.btech@gmail.com>
2025-01-30 11:52:44 +0530

    smartlook added for app only

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,27 @@
 <script setup lang="ts">
 import { Smartlook } from '@awesome-cordova-plugins/smartlook';
 import { onMounted } from 'vue';
+import { Capacitor } from '@capacitor/core';
 
 // Set up Smartlook with your API key
 onMounted(async () => {
-  Smartlook.setProjectKey({ key: '056d4905ebd5ce473f5ad5fa953788424054cd60' });
-  Smartlook.start();
-  console.log('smartlook hello');
-  const isRecording = await Smartlook.isRecording();
-  console.log('isRecording', isRecording);
+  if (
+    Capacitor.getPlatform() === 'android' ||
+    Capacitor.getPlatform() === 'ios'
+  ) {
+    try {
+      Smartlook.setProjectKey({
+        key: process.env.SMARTLOOK_PROJECT_KEY || '',
+      });
+      Smartlook.start();
+      console.log('smartlook hello');
+      const isRecording = await Smartlook.isRecording();
+      console.log('smartlook isRecording', isRecording);
+    } catch (Error) {
+      console.log('Smartlook erro is not initialized', Error);
+    }
+  } else {
+    console.log('Smartlook is not initialized on web or PWA');
+  }
 });
 </script>
